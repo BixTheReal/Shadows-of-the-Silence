8299c4e0e94f2be1114102011a94b7c4400ae3b5
BixTheReal <bixanschreiben@gmail.com>
2025-05-10 15:11:29 +0200

    - Point Light for Light Block (WIP)

--- src/main/java/bixstudio/sots/block/LightBlockBlock.java
+++ src/main/java/bixstudio/sots/block/LightBlockBlock.java
@@ -1,12 +1,16 @@
 package bixstudio.sots.block;
 
 import bixstudio.sots.blockentity.LightBlockBE;
-import bixstudio.sots.blockentity.SprayedWallBE;
+import bixstudio.sots.blockentity.ModBlockEntities;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
+import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 public class LightBlockBlock extends BlockWithEntity {
 
--- src/main/java/bixstudio/sots/blockentity/LightBlockBE.java
+++ src/main/java/bixstudio/sots/blockentity/LightBlockBE.java
@@ -1,12 +1,42 @@
 package bixstudio.sots.blockentity;
 
+import foundry.veil.api.client.render.VeilRenderSystem;
+import foundry.veil.api.client.render.light.PointLight;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.joml.Vector3d;
 
-public class LightBlockBE extends BlockEntity {
+
+public class LightBlockBE extends BlockEntity implements BlockEntityTicker {
+
+    public PointLight pointLight;
 
     public LightBlockBE(BlockPos pos, BlockState state) {
         super(ModBlockEntities.LIGHT_BLOCK_BE, pos, state);
+    }
+
+    @Override
+    public void tick(World world, BlockPos pos, BlockState state, BlockEntity blockEntity) {
+        if (world == null || !world.isClient) return;
+
+        if (pointLight == null){
+            pointLight = new PointLight();
+            pointLight.setPosition(new Vector3d(pos.getX(), pos.getY() - 1, pos.getZ()));
+            pointLight.setColor(0xFFFFBB33);
+            pointLight.setBrightness(2f);
+            VeilRenderSystem.renderer().getLightRenderer().addLight(pointLight);
+        }
+    }
+
+    @Override
+    public void markRemoved() {
+        super.markRemoved();
+        if (pointLight != null) {
+            VeilRenderSystem.renderer().getLightRenderer().removeLight(pointLight);
+            pointLight = null;
+        }
     }
 }
